3dcc0d5a86b81ed0a6905a856d235fac52382b4c
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2024-04-14 04:44:39 +0800

    patch: taglayouts

--- config.def.h
+++ config.def.h
@@ -29,7 +29,8 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] =     { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
+static const int taglayouts[] = {  0,   2,   0,   0,   0,   0,   0,   2,   0,   0 };
 
 static const Rule rules[] = {
 	/* xprop(1):
