cc097d3bf39288c07b37559c9a77f5f1cfbc820d
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2024-04-13 19:29:40 +0800

    patch: exitmenu

--- config.def.h
+++ config.def.h
@@ -1,5 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 
+#include "exitdwm.c"
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -109,7 +110,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,      exitdwm,        {0} },
 };
 
 /* button definitions */
