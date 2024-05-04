b63145df36a1b22e1474021530123b8154aa7f43
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2024-05-04 09:05:51 +0800

    patch: togglefloatingcenter

--- dwm.c
+++ dwm.c
@@ -2395,6 +2395,10 @@ togglefloating(const Arg *arg)
 		selmon->sel->sfw = selmon->sel->w;
 		selmon->sel->sfh = selmon->sel->h;
 	}
+
+	selmon->sel->x = selmon->sel->mon->mx + (selmon->sel->mon->mw - WIDTH(selmon->sel)) / 2;
+	selmon->sel->y = selmon->sel->mon->my + (selmon->sel->mon->mh - HEIGHT(selmon->sel)) / 2;
+
 	arrange(selmon);
 }
 
