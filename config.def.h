2828d92e5e1fc50a4b2470ffbda47ce30bc57f25
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2025-07-08 20:23:30 +0800

    chore: add pulsemixer window rule

--- config.def.h
+++ config.def.h
@@ -90,6 +90,7 @@ static const Rule rules[] = {
 	{ "St",                       NULL,                   "bluetuith",               0,          0,           1,           -1,          0 },
 	{ "St",                       NULL,                   "xprop",					 0,          0,           1,           -1,          0 },
 	{ "St",                       NULL,                   "ncpamixer",				 0,          0,           1,           -1,          0 },
+	{ "St",                       NULL,                   "pulsemixer",				 0,          0,           1,           -1,          0 },
 	{ "Xdg-desktop-portal-gtk",   NULL,                   NULL,                      0,          1,           1,           -1,          0 },
 	{ "gnome-system-monitor",     NULL,                   NULL,                      0,          1,           1,           -1,          0 },
 	{ "gnome-calculator",         NULL,                   NULL,                      0,          1,           1,           -1,          0 },
